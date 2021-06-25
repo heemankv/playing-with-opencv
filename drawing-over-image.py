78a3a8bae1932a2358fe98eaced61e78db522a0a
Heemank Verma <vheemank@gmail.com>
2021-06-25 16:21:23 +0530

    drawing 1 done

--- drawing-over-image.py
+++ drawing-over-image.py
@@ -11,5 +11,14 @@ cv2.rectangle(blank_img, pt1=(384, 0), pt2=(
 cv2.rectangle(blank_img, pt1=(200, 200), pt2=(
     250, 250), color=(0, 255, 0), thickness=7)
 
+cv2.circle(img=blank_img, center=(100, 100),
+           radius=50, color=(0, 255, 0), thickness=10)
+
+cv2.circle(img=blank_img, center=(400, 400),
+           radius=50, color=(0, 255, 0), thickness=-1)
+
+cv2.line(blank_img, pt1=(0, 0), pt2=(512, 512),
+         color=(0, 255, 0), thickness=1)
+
 plt.imshow(blank_img)
 plt.show()
