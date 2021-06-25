64dd498d4f3de1154c966d0e8c90e8debfa234a0
Heemank Verma <vheemank@gmail.com>
2021-06-25 17:07:16 +0530

    drawing+counting using mouse

--- drawing-with-mouse.py
+++ drawing-with-mouse.py
@@ -2,11 +2,24 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+count = 0
+
 
 def draw_circle(event, x, y, flags, param):
+    global count
 
     if event == cv2.EVENT_LBUTTONDOWN:
         cv2.circle(img, (x, y), 80, (0, 255, 0), -1)
+
+        count += 1
+
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img, (x, y), 80, (0, 0, 255), -1)
+        count += 1
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        cv2.circle(img, (x, y), 80, (0, 0, 0), -1)
+        count += 1
 
 
 cv2.namedWindow(winname='my_drawing')
@@ -23,3 +36,5 @@ while True:
         break
 
 cv2.destroyAllWindows()
+
+print(count)
