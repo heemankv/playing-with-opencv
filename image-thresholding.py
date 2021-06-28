8168906a5c3a2b8448cac09d731b358f1f6cae91
Heemank Verma <vheemank@gmail.com>
2021-06-28 18:35:19 +0530

    image thresholding basic

--- image-thresholding.py
+++ image-thresholding.py
@@ -3,5 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-img2 = cv2.imread(
-    '/home/malcroft/Documents/Computer-Vision-with-Python/Computer-Vision-with-Python/DATA/watermark_no_copy.png')
+img = cv2.imread(
+    '/home/malcroft/Documents/Computer-Vision-with-Python/Computer-Vision-with-Python/DATA/rainbow.jpg', 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+print(ret)
+plt.imshow(thresh1, cmap='gray')
+plt.show()
