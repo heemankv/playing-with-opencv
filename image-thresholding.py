4f4a3d0440df6415df28e2451816f033c5583f3c
Heemank Verma <vheemank@gmail.com>
2021-06-28 18:53:28 +0530

    image thresholding

--- image-thresholding.py
+++ image-thresholding.py
@@ -4,12 +4,25 @@ import matplotlib.pyplot as plt
 
 
 img = cv2.imread(
-    '/home/malcroft/Documents/Computer-Vision-with-Python/Computer-Vision-with-Python/DATA/rainbow.jpg', 0)
+    '/home/malcroft/Documents/Computer-Vision-with-Python/Computer-Vision-with-Python/DATA/crossword.jpg', 0,)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = img.astype(np.uint8)
 
 
-ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+def show_pic(img):
+    fig = plt.figure(figsize=(15, 15))
+    ax = fig.add_subplot(111)
+    ax.imshow(img, cmap='gray')
+    plt.show()
 
-print(ret)
-plt.imshow(thresh1, cmap='gray')
-plt.show()
+
+# not a very good way
+ret, th1 = cv2.threshold(img, 170, 255, cv2.THRESH_BINARY)
+
+# need to get an addaptive thresholding
+
+th2 = cv2.adaptiveThreshold(
+    img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 8)
+
+
+show_pic(th2)
